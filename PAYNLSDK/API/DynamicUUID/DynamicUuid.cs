f3fce3ba3d8429e13db2d10b1f6267ad688602d8
Damiaan Peeters <damiaan@umbrace.be>
2021-08-24 20:11:25 +0200

    Convert PHP from dynamic uuid to native c#

--- PAYNLSDK/API/DynamicUUID/DynamicUuid.cs
+++ PAYNLSDK/API/DynamicUUID/DynamicUuid.cs
@@ -18,7 +18,8 @@ namespace PayNLSdk.API.DynamicUUID
     {
         private const int REFERENCE_TYPE_STRING = 1;
         private const int REFERENCE_TYPE_HEX = 0;
-        private const string HASH_METHOD = "sha256";
+
+        private static readonly Encoding Encoding = Encoding.UTF8;
 
         /**
          * Generate a UUID
@@ -35,167 +36,249 @@ namespace PayNLSdk.API.DynamicUUID
         {
             if (referenceType == REFERENCE_TYPE_STRING)
             {
-                this.validateReferenceString(reference);
+                validateReferenceString(reference);
                 reference = this.asciiToHex(reference);
             }
-            else if (referenceType == this.REFERENCE_TYPE_HEX)
+            else if (referenceType == REFERENCE_TYPE_HEX)
             {
-                this.validateReferenceHex(reference);
+                validateReferenceHex(reference);
             }
-            this.validateSecret(secret);
-            this.validateServiceId(serviceId);
-            this.validatePadChar(padChar);
-            var UUIDData = preg_replace('/[^0-9]/', '', serviceId);
-            UUIDData+= str_pad(strtolower(reference), 16, padChar, STR_PAD_LEFT);
-            hash = hash_hmac(this.HASH_METHOD, UUIDData, secret);
-            UUID = "b".substr(hash, 0, 7).UUIDData;
-            return sprintf(
-                '%08s-%04s-%04s-%04s-%12s',
-                substr(UUID, 0, 8),
-                substr(UUID, 8, 4),
-                substr(UUID, 12, 4),
-                substr(UUID, 16, 4),
-                substr(UUID, 20, 12)
+            validateSecret(secret);
+            validateServiceId(serviceId);
+            validatePadChar(padChar);
+            var UUIDData = System.Text.RegularExpressions.Regex.Replace(serviceId, "/[^0-9]/", "");
+            UUIDData += reference.ToLowerInvariant().PadLeft(16, padChar);
+
+            var hash = MhmacSHA256(UUIDData, secret);
+            var UUID = "b" + hash.Substring(0, 7) + UUIDData;
+            return string.Format(
+                "%08s-%04s-%04s-%04s-%12s",
+                UUID.Substring(0, 8),
+                UUID.Substring(8, 4),
+                UUID.Substring(12, 4),
+                UUID.Substring(16, 4),
+                UUID.Substring(20, 12)
             );
         }
+
+        #region new helper functions
+        public static string MhmacSHA256(string key, string message)
+        {
+            var keyByte = Encoding.GetBytes(key);
+            using (var hmacsha256 = new System.Security.Cryptography.HMACSHA256(keyByte))
+            {
+                hmacsha256.ComputeHash(Encoding.GetBytes(message));
+
+                return ByteToString(hmacsha256.Hash);
+            }
+        }
+
+        private static string ByteToString(byte[] buff)
+        {
+            var sbinary = "";
+            for (int i = 0; i < buff.Length; i++)
+            {
+                sbinary += buff[i].ToString("X2"); /* hex format */
+            }
+            return sbinary;
+        }
+
+        // original from https://stackoverflow.com/a/29950264/97615
+        private static string packHStar(string input)
+        {
+            //only for H32 & H*
+            return Encoding.Default.GetString(FromHex(input));
+        }
+
+        // original from https://stackoverflow.com/a/29950264/97615
+        private static byte[] FromHex(string hex)
+        {
+            hex = hex.Replace("-", "");
+            byte[] raw = new byte[hex.Length / 2];
+            for (int i = 0; i < raw.Length; i++)
+            {
+                raw[i] = Convert.ToByte(hex.Substring(i * 2, 2), 16);
+            }
+            return raw;
+        }
+        // original from https://stackoverflow.com/a/29950264/97615
+        private static string unpack(string p1, string input)
+        {
+            StringBuilder output = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string a = Convert.ToInt32(input[i]).ToString("X");
+                output.Append(a);
+            }
+
+            return output.ToString();
+        }
+
+        /// <summary>
+        /// The equivalent for hexdec in php
+        /// </summary>
+        /// <param name="hex"></param>
+        /// <returns></returns>
+        private static int HexToDec(String hex)
+        {
+            //Int32.Parse(hexString, System.Globalization.NumberStyles.HexNumber);
+            return Convert.ToInt32(hex, 16);
+        }
+
+        /// <summary>
+        /// The equivalent for dechex in php
+        /// </summary>
+        /// <param name="decValue"></param>
+        /// <returns></returns>
+        private static string DecToHex(int? decValue)
+        {
+            return decValue == null ? "" : decValue.Value.ToString("X");
+        }
+
+        #endregion
+
         private string asciiToHex(string ascii)
         {
             var hex = "";
             for (var i = 0; i < ascii.Length; i++)
             {
-                byte = strtoupper(dechex(ord(ascii{ i})));
-            byte = str_repeat('0', 2 - strlen(byte)).byte;
-            hex.= byte."";
+                var b = DecToHex((int)ascii[i]).ToUpper();
+                b = new string('0', 2 - b.Length) + b;
+                hex += b;
+            }
+            return hex;
         }
-        return hex;
-    }
-    private void validateSecret(string strSecret)
-    {
-        if (!preg_match('/^[0-9a-f]{40}/i', strSecret))
+
+        private void validateSecret(string strSecret)
         {
-            throw new Error("Invalid secret");
+            if (!System.Text.RegularExpressions.Regex.IsMatch(strSecret, "/^[0-9a-f]{40}/", System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+            {
+                throw new PayNlException("Invalid secret");
+            }
         }
-    }
-    private void validateServiceId(string strServiceId)
-    {
-        if (!preg_match('/^SL-[0-9]{4}-[0-9]{4}/', strServiceId))
+        private void validateServiceId(string strServiceId)
         {
-            throw new PayNlException("Invalid service ID");
+            if (!System.Text.RegularExpressions.Regex.IsMatch(strServiceId, "/^SL-[0-9]{4}-[0-9]{4}/"))
+            {
+                throw new PayNlException("Invalid service ID");
+            }
         }
-    }
-    private void validateReferenceString(string strReference)
-    {
-        if (!preg_match('/^[0-9a-zA-Z]{0,8}/i', strReference))
+        private void validateReferenceString(string strReference)
         {
-            throw new PayNlException("Invalid reference: only alphanumeric chars are allowed, up to 8 chars long");
+            if (!System.Text.RegularExpressions.Regex.IsMatch(strReference, "/^[0-9a-zA-Z]{0,8}/", System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+            {
+                throw new PayNlException("Invalid reference: only alphanumeric chars are allowed, up to 8 chars long");
+            }
         }
-    }
-    private static function validateReferenceHex(string strReference)
-    {
-        if (!preg_match('/^[0-9a-f]{0,16}/i', strReference))
+        private void validateReferenceHex(string strReference)
         {
-            throw new PayNlException("Invalid reference: only alphanumeric chars are allowed, up to 16 chars long");
+            if (!System.Text.RegularExpressions.Regex.IsMatch(strReference, "/^[0-9a-f]{0,16}/", System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+            {
+                throw new PayNlException("Invalid reference: only alphanumeric chars are allowed, up to 16 chars long");
+            }
         }
-    }
-    private static function validatePadChar(strPadChar)
-    {
-        if (!preg_match('/^[a-z0-9]{1}/i', strPadChar))
+        private void validatePadChar(char strPadChar)
         {
-            throw new PayNlException('Invalid pad char');
+            if (!System.Text.RegularExpressions.Regex.IsMatch(new string(new[] { strPadChar }), "/^[a-z0-9]{1}/", System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+            {
+                throw new PayNlException("Invalid pad char");
+            }
         }
-    }
-    /**
-     * Get url and qr-image for bancontact
-     * @param UUID
-     * @param withBase64 True if you need a base64 image
-     * @return array url, QRUrl and QRBase64
-     */
-    public new Dictionary<string, string> bancontact(string UUID, bool withBase64 = false)
-    {
-        string qrUrl = "https://chart.googleapis.com/chart?cht=qr&chs=260x260&chl=https://qr.pisp.me/bc/" + UUID;
-        var result = new Dictionary<string, string>()
+
+        /**
+         * Get url and qr-image for bancontact
+         * @param UUID
+         * @param withBase64 True if you need a base64 image
+         * @return array url, QRUrl and QRBase64
+         */
+        public Dictionary<string, string> bancontact(string UUID, bool withBase64 = false)
+        {
+            string qrUrl = "https://chart.googleapis.com/chart?cht=qr&chs=260x260&chl=https://qr.pisp.me/bc/" + UUID;
+            var result = new Dictionary<string, string>()
         {
             {"url" , "https://qr.pisp.me/bc/" + UUID},
             { "QRUrl" , qrUrl}
 
         };
-        if (withBase64) result["QRBase64"] = base64_encode(file_get_contents(qrUrl));
-        return result;
-    }
+            if (withBase64) result["QRBase64"] = base64_encode(file_get_contents(qrUrl));
+            return result;
+        }
 
-    /**
-     * Get url and qr-image for ideal
-     *
-     * @param UUID
-     * @param withBase64 True if you need a base64 image
-     * @return array url, QRUrl and QRBase64
-     */
-    public new Dictionary<string, string> ideal(string UUID, bool withBase64 = false)
-    {
-        var qrUrl = "https://ideal.pay.nl/qr/" + UUID;
-        var result = new Dictionary<string, string>()
+        /**
+         * Get url and qr-image for ideal
+         *
+         * @param UUID
+         * @param withBase64 True if you need a base64 image
+         * @return array url, QRUrl and QRBase64
+         */
+        public new Dictionary<string, string> ideal(string UUID, bool withBase64 = false)
+        {
+            var qrUrl = "https://ideal.pay.nl/qr/" + UUID;
+            var result = new Dictionary<string, string>()
         {
             {"url" , "https://qr6.ideal.nl/" + UUID},
             { "QRUrl" , qrUrl}
 
         };
 
-        if (withBase64) result["QRBase64"] = base64_encode(file_get_contents(qrUrl));
-        return result;
-    }
-    /**
-     * Decode a UUID
-     *
-     * @param string uuid The UUID to decode
-     * @param string|null secret If supplied the uuid will be validated before decoding.
-     * @param string padChar The reference will be padded with this character, default '0'
-     *
-     * @return array Array with serviceId and reference
-     * @throws Error
-     */
-    public Dictionary<string, string> decode(string uuid, string secret = null, char padChar = '0', int referenceType = REFERENCE_TYPE_STRING)
-    {
-        if (string.IsNullOrWhiteSpace(secret) == false)
-        {
-            this.validateSecret(secret);
-            isValid = this.validate(uuid, secret);
-            if (!isValid)
-            {
-                throw new Error("Incorrect signature");
-            }
-        }
-        var uuidData = preg_replace('/[^0-9a-z]/i', "", uuid);
-        uuidData = substr(uuidData, 8);
-        var serviceId = "SL-" + substr(uuidData, 0, 4) +  '-' + substr(uuidData, 4, 4);
-        var reference = substr(uuidData, 8);
-        reference = ltrim(reference, padChar);
-        if (referenceType == this.REFERENCE_TYPE_STRING)
-        {
-            reference = pack("H*", reference);
+            if (withBase64) result["QRBase64"] = base64_encode(file_get_contents(qrUrl));
+            return result;
         }
 
-        return Dictionary<string, string>{
-            { "serviceId" , serviceId}
+        /**
+         * Decode a UUID
+         *
+         * @param string uuid The UUID to decode
+         * @param string|null secret If supplied the uuid will be validated before decoding.
+         * @param string padChar The reference will be padded with this character, default '0'
+         *
+         * @return array Array with serviceId and reference
+         * @throws Error
+         */
+        public Dictionary<string, string> decode(string uuid, string secret = null, char padChar = '0', int referenceType = REFERENCE_TYPE_STRING)
+        {
+            if (string.IsNullOrWhiteSpace(secret) == false)
+            {
+                validateSecret(secret);
+                var isValid = this.validate(uuid, secret);
+                if (!isValid)
+                {
+                    throw new PayNlException("Incorrect signature");
+                }
+            }
+
+            var uuidData = System.Text.RegularExpressions.Regex.Replace(uuid, "/[^0-9a-z]/", "", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            uuidData = uuidData.Substring(8);
+            var serviceId = "SL-" + uuidData.Substring(0, 4) + '-' + uuidData.Substring(4, 4);
+            var reference = uuidData.Substring(8);
+            reference = reference.TrimStart(padChar); // trim leading characters
+            if (referenceType == REFERENCE_TYPE_STRING)
+            {
+                reference = packHStar(reference);
+            }
+
+            return new Dictionary<string, string>{
+            { "serviceId" , serviceId},
             { "reference", reference}
+        };
         }
-        ;
+
+        /**
+         * Validate a UUID with supplied secret
+         *
+         * @param string uuid
+         * @param string secret
+         *
+         * @return bool
+         */
+        public bool validate(string uuid, string secret)
+        {
+            var uuidData = System.Text.RegularExpressions.Regex.Replace(uuid, "/[^0-9a-z]/", "", System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            uuidData = uuidData.Substring(8);
+            var hash = MhmacSHA256(secret, uuidData);
+            var checksum = "b" + hash.Substring(0, 7);
+            return checksum == uuid.Substring(0, 8);
+        }
     }
-    /**
-     * Validate a UUID with supplied secret
-     *
-     * @param string uuid
-     * @param string secret
-     *
-     * @return bool
-     */
-    public
-    static function validate(uuid, secret)
-    {
-        uuidData = preg_replace('/[^0-9a-z]/i', '', uuid);
-        uuidData = substr(uuidData, 8);
-        hash = hash_hmac(this.HASH_METHOD, uuidData, secret);
-        checksum = "b".substr(hash, 0, 7);
-        return checksum == substr(uuid, 0, 8);
-    }
-}
 }
