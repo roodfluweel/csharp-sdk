af42d01eee3ad98efd3080bfc77034dbc605b298
Damiaan Peeters <damiaan@umbrace.be>
2021-09-20 23:03:46 +0200

    Adding document/Add support
    fixes #17

---
+++ PAYNLSDK/API/Document/Add/Request.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+using PAYNLSDK.API;
+using PAYNLSDK.Exceptions;
+using PAYNLSDK.Utilities;
+
+namespace PayNLSdk.API.Document.Add
+{
+    /// <summary>
+    /// Upload one or multiple files to a document for a merchant or account.
+    /// </summary>
+    public class Request : RequestBase
+    {
+        /// <inheritdoc />
+        protected override int Version => 1;
+        /// <inheritdoc />
+        protected override string Controller => "document";
+        /// <inheritdoc />
+        protected override string Method => "add";
+
+        /// <summary>
+        /// The id of the document
+        /// </summary>
+        [Required]
+        public string DocumentId { get; set; }
+
+        /// <summary>
+        /// the name (and extension) of the file you're adding.
+        /// </summary>
+        [Required]
+        public string FileName { get; set; }
+
+        /// <summary>
+        /// The content of the file which should be uploaded
+        /// </summary>
+        [Required]
+        public List<byte[]> FileBytes { get; set; }
+
+        /// <inheritdoc />
+        public override NameValueCollection GetParameters()
+        {
+            NameValueCollection nvc = new NameValueCollection();
+            ParameterValidator.IsNotEmpty(DocumentId, "DocumentId");
+            ParameterValidator.IsNotEmpty(FileName, "DocumentId");
+            ParameterValidator.IsNotNull(FileBytes, "FileBytes");
+
+            nvc.Add("documentId", DocumentId);
+            nvc.Add("filename", FileName);
+
+            //nvc.Add("documentFile", Convert.ToBase64String(FileBytes));
+            for (var i = 0; i < FileBytes.Count; i++)
+            {
+                var fileBytes = FileBytes[i];
+                nvc.Add($"documentFile[{i}]", Convert.ToBase64String(fileBytes));
+            }
+
+            return nvc;
+        }
+
+        /// <inheritdoc />
+        protected override void PrepareAndSetResponse()
+        {
+            if (ParameterValidator.IsEmpty(rawResponse))
+            {
+                throw new PayNlException("rawResponse is empty!");
+            }
+
+            var r = JsonConvert.DeserializeObject<Response>(RawResponse);
+            response = r;
+        }
+    }
+}
